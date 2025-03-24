8d3f81449d022c03dcd9b09e641a7f6f1a38bbc3
JordiNodejs <jordi.nodejs@gmail.com>
2025-03-24 16:53:29 +0100

    add graceful shutdown handling on SIGTERM signal

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,5 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(3100);
+
+  // Captura la señal de terminación y cierra la aplicación
+  process.on('SIGTERM', async () => {
+    // SIGTERM es una señal que se envía a un proceso para indicarle que debe terminar.
+    await app.close();
+    process.exit(0);
+  });
 }
 bootstrap();
